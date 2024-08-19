11f8e73d62e5137d42c35250149bfdcc19401788
Pon Channarith <ponchannarith@gmail.com>
2024-08-19 21:33:33 +0700

    update code

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,3 +1,13 @@
+// export const accessToken = localStorage.getItem("access_token");
+
+// export const AUTH_HEADER = {
+//   Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+// };
+
+// // Use the AUTH_HEADER to make authenticated requests
+// export const API_BASE_URI = "http://136.228.158.126:50001/api/";
+
+
 export const accessToken = localStorage.getItem("access_token");
 
 export const AUTH_HEADER = {
@@ -5,5 +15,5 @@ export const AUTH_HEADER = {
 };
 
 // Use the AUTH_HEADER to make authenticated requests
-export const API_BASE_URI = "http://136.228.158.126:50001/api/";
-
+// export const API_BASE_URI = "http://136.228.158.126:50001/api/";
+export const API_BASE_URI = "https://stem.automatex.dev/api/";
